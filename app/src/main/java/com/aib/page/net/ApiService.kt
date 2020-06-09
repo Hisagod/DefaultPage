169d8537138f3392661424bd192a71651fba5ddb
HisGod <1160860077@qq.com>
2020-06-09 11:50:34 +0800

    fix:接口参数注释

--- app/src/main/java/com/aib/page/net/ApiService.kt
+++ app/src/main/java/com/aib/page/net/ApiService.kt
@@ -6,6 +6,9 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
+    /**
+     * count: -1(返回出错数据)    0(不反返回数据)   大于0（返回几条数据）
+     */
     @GET("demo/getOne")
     suspend fun getOne(@Query("count") count: Int): BaseBean<List<PersonBean>>
 }
